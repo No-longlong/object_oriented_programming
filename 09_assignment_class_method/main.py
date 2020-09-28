5b49aad02d707f2d4c73c7d07d6b4e6ccdf13ac5
longTimeNoSee <johnberboy@naver.com>
2020-09-28 10:32:21 +0900

    Assignment about class method

---
+++ 09_assignment_class_method/main.py
@@ -0,0 +1,26 @@
+class User:
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+    @classmethod
+    def from_string(cls, string_params):
+        split_user = string_params.split(",")
+        name = split_user[0]
+        email = split_user[1]
+        password = split_user[2]
+        return cls(name, email, password)
+
+    @classmethod
+    def from_list(cls, list_params):
+        name = list_params[0]
+        email = list_params[1]
+        password = list_params[2]
+        return cls(name, email, password)
+# 유저 생성 및 초기값 설정
+younghoon = User.from_string("강영훈,younghoon@codeit.kr,123456")
+yoonsoo = User.from_list(["이윤수", "yoonsoo@codeit.kr", "abcdef"])
+
+print(younghoon.name, younghoon.email, younghoon.password)
+print(yoonsoo.name, yoonsoo.email, yoonsoo.password)
