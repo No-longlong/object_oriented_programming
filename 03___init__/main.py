b6e3ad631b2cddbde6a1f2e41c38a6e1f3609954
longTimeNoSee <johnberboy@naver.com>
2020-10-15 12:41:16 +0900

    Updated explanation

--- 03___init__/main.py
+++ 03___init__/main.py
@@ -1,8 +1,9 @@
 class User:
-    def __init__(self, n, m, p):
+    def __init__(self, n, m, p): # magic method, special method (특수 메소드)
         self.name = n
         self.email = m
         self.password = p
+# 인스턴스가 생성될 때 자동으로 호출
 # class를 호출하자마자, __init__이 실행되면서
 # 받은 파라미터를 통해 인스턴스 변수를 만들어주는 행위를 한다.
 
@@ -13,6 +14,12 @@ user2 = User("Yoonsoo", "yoonsoo@codeit.kr", "abcdef")
 user3 = User("Taeho", "taeho@codeit.kr", "123abc")
 
 user4 = User("Lisa", "lisa@codeit.kr", "abc123")
+
+# 순서
+# 1. User 인스턴스(user1) 생성
+# 2. __init__ 메소드 자동 호출
+
+# init 메소드가 인스턴스 생성과 인스턴스 변수 초깃값 호출을 한줄에 할 수 있도록 만듬
 
 
 
