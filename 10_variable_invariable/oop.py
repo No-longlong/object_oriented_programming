76d101813b81ca710f3e8e7b6ba1e800f4adba8b
longTimeNoSee <johnberboy@naver.com>
2020-10-30 08:49:01 +0900

    invariable object

--- 10_variable_invariable/oop.py
+++ 10_variable_invariable/oop.py
@@ -1,27 +1,27 @@
 # # 가변 타입(리스트) vs 불변 타입(튜플)
 #
-# mutable_object = [1, 2, 3]
-# immutable_object = (1, 2, 3)
-#
-# mutable_object[0] = 4
-# print(mutable_object)
-#
-# immutable_object[0] = 4
-# print(immutable_object) # 에러
-#
+mutable_object = [1, 2, 3]
+immutable_object = (1, 2, 3)
+
+mutable_object[0] = 4
+print(mutable_object)
+
+immutable_object[0] = 4
+print(immutable_object) # 에러
 
 
-# tuple_x = (6, 4)
-# tuple_x[0] = 4
-# tuple_x[1] = 1
-#
-# print(tuple_x) # 이미 생성된 객체의 값을 바꾸는 것은 에러
 
-# tuple_x = (6, 4)
-# tuple_x = (4, 1)
-# tuple_x = (4, 1, 7)
-#
-# print(tuple_x)
+tuple_x = (6, 4)
+tuple_x[0] = 4
+tuple_x[1] = 1
+
+print(tuple_x) # 이미 생성된 객체의 값을 바꾸는 것은 에러
+
+tuple_x = (6, 4)
+tuple_x = (4, 1)
+tuple_x = (4, 1, 7)
+
+print(tuple_x)
 
 # 정해진 튜플의 속성을 바꾸는 것은 에러 [(4, 1) --> 0번째를 3으로 바꿔라 (X)]
 # 같은 변수 이름으로 튜플을 새롭게 할당하는 것은 가능
